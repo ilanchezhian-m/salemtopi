3fa31670076f5234f8097beefcca0a8a49005dbb
Ilanchezhian <you@example.com>
2024-10-23 12:17:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -494,6 +494,57 @@ function App() {
       <p>Mutton Ball Machurian</p>
       <p>₹280</p>
     </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Pallipalayam</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Drumstick (2pcs) Saucy </p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Chicken Dry</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Manchurian</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken 65 (Boneless) (8pcs)</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Lollipop(5pcs)</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Pepper Fry</p>
+      <p>₹210</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Dragon Chicken Dry</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Crispy Fish Finger(6 pcs)</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Chukka</p>
+      <p>₹210</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Crab Lollipop(5 pcs)</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mutton Chukka</p>
+      <p>₹270</p>
+    </div>
+   
+
+    
 
 
 
@@ -501,12 +552,571 @@ function App() {
   </li>
 </ul>
 </div>
+{/*veg Starters */}
+<div className="flex items-center ml-2">
+    <h2 className='my-1 font-bold'> VEG STARTERS</h2>  
+    <img src={veg} alt="non veg" className="w-8 h-auto " />
+  </div>
+  
+  <div className='ml-2'>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Chilli Gobi Dry</p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Gobi Manchurian</p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Gobi 65</p>
+      <p>₹140</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Mushroom Dry
+      </p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Panner Dry
+      </p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mushroom Manchurian</p>
+      <p>₹170</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mushroom 65</p>
+      <p>₹160</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner Manchurian</p>
+      <p>₹160</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner 65</p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Baby Corn Dry
+      </p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Golden Fried Baby Corn</p>
+      <p>₹170</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Golden Fried Panner</p>
+      <p>₹180</p>
+    </div>
+  </li>
+</ul>
+</div>
+{/* Tandoori */}
+<div className="flex items-center ml-2">
+    <h2 className='my-1 font-bold'>TANDOORI</h2>  
+    <img src={nonveg} alt="non veg" className="w-8 h-auto " />
+  </div>  
+
+  <div className='ml-2'>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Chicken Quarter</p>
+      <p>₹110</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Chicken Half</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Chicken Full</p>
+      <p>₹400</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Topi Kebab</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tangiri Kebab</p>
+      <p>200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Tikka</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Spicy Tikka</p>
+      <p>₹200</p>
+      </div>
+      <div className="flex justify-between mr-2">
+      <p>Reshmi Kebab</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Seekh Kebab</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Malai Chicken Tikka</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Hariyali Chicken Tikka</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner Tikka</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mutton Seekh Kebab</p>
+      <p>₹270</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Fish Tikka</p>
+      <p>₹240</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Malabar Tikka</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Garlic Kebab</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Anghari Kebab </p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kali Mirchi Kebab</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Hariyali Panner Tikka</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Cheese Kebab</p>
+      <p>₹210</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Honey Chicken Kebab</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Platter</p>
+      <p>₹599</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Family Kebab Platter</p>
+      <p>₹699</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Fish</p>
+      <p>₹280</p>
+    </div>
+    
+    {/* Non Veg Indian Curries */}
+
+    
 
 
 
+    
+  </li>
+</ul>
+</div>
+{/* Non veg Indian Curries */}
+<div className="flex items-center ml-2">
+    <h2 className='my-1 font-bold'>NON VEG INDIAN CURRIES</h2>  
+    <img src={nonveg} alt="non veg" className="w-8 h-auto " />
+  </div>  
 
+<div className='ml-2'>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Chicken Chettinad</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Pepper Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Butter Chicken Masala</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kadai Chicken</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Andhra Chicken Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Hyderabadi Chicken Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Mughalai</p>
+      <p>₹230</p>
+      </div>
+      <div className="flex justify-between mr-2">
+      <p>Kadai Mutton</p>
+      <p>₹260</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mutton Chettinad</p>
+      <p>₹270</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mutton Pepper Masala</p>
+      <p>₹260</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Tikka Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Curry</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Tikka Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Curry</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kerala Chicken Masala</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Nattu Kozhi Pepper Masala </p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Nattu Kozhi Chukka</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kaadai Pepper Masala</p>
+      <p>₹230</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kadai Cukka </p>
+      <p>₹230</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Punjabi Chicken Masala</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Kolapuri</p>
+      <p>₹230</p>
+    </div>
+    {/*VEG INDIAN CURRIES */}
+    
+   
+    
+  
 
+    
+  </li>
+</ul>
+</div>
 
+{/* veg Indian Curries */}
+<div className="flex items-center ml-2">
+    <h2 className='my-1 font-bold'>VEG INDIAN CURRIES</h2>  
+    <img src={veg} alt="non veg" className="w-8 h-auto " />
+  </div>  
+
+<div className='ml-2'>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Gobi Masala</p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Mushroom Masala</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner Butter Masala</p>
+      <p>₹190</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kadai Panner</p>
+      <p>₹200</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Veg Chetinadu</p>
+      <p>₹180</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner Tikka Masala</p>
+      <p>₹200</p>
+      </div>
+      <div className="flex justify-between mr-2">
+      <p>Mix Veg Gravy</p>
+      <p>₹170</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Green Peas Masala</p>
+      <p>₹140</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kadai Veg</p>
+      <p>₹160</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Veg Kolapuri</p>
+      <p>₹150</p>
+    </div>
+    
+    {/*EGG DISHES  */}
+{/* Non veg Indian Curries */}
+<div className="flex items-center ">
+    <h2 className='my-1 font-bold'>EGG DISH</h2>  
+    <img src={nonveg} alt="non veg" className="w-8 h-auto " />
+  </div>  
+
+<div className='ml-1'>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Boiled Egg</p>
+      <p>₹10</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Egg Masala</p>
+      <p>₹130</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Egg Burgi</p>
+      <p>₹50</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Egg 65</p>
+      <p>₹100</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Egg Podimas</p>
+      <p>₹50</p>
+    </div>
+    
+  
+  {/*SEA FOODS */}
+  <div className="flex items-center ">
+    <h2 className='my-1 font-bold'>SEA FOODS</h2>  
+    <img src={nonveg} alt="non veg" className="w-8 h-auto " />
+  </div>  
+
+<div className=''>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Vanjaram Fish Tawa Fry</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Fish Masala</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Fish Munchurian</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Nethili Fish Fry</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Fish 65</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Fish Fry</p>
+      <p>₹240</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Schezwan Fish Fry</p>
+      <p>₹250</p>
+      </div>
+      <div className="flex justify-between mr-2">
+      <p>Crab Lollipop(5 pcs)</p>
+      <p>₹220</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Prawn 65</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Prawn Manchurian</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chilli Prawn Dry</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Schezwan Prawn</p>
+      <p>₹260</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Prawn Chettinadu</p>
+      <p>₹260</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Sheela Fish Fry</p>
+      <p>₹230</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Glass Para Fish Full(Tawa or Oil Fry)</p>
+      <p>₹550</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Crab Pepper Fry </p>
+      <p>₹300</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Dragon Fish Fry</p>
+      <p>₹250</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>BBQ Fish(3 Flawars)</p>
+      <p>₹300</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Dragon Prawn Strips </p>
+      <p>₹350</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Golden Fry Prawns</p>
+      <p>₹280</p>
+    </div>
+   
+    {/*VEG INDIAN CURRIES */}
+    
+    
+    
+  
+
+    
+  </li>
+</ul>
+</div>
+    {/* INDIAN BREADS */}
+    <div className="flex items-center">
+    <h2 className='my-1 font-bold'>INDIAN BREADS</h2>  
+    <img src={nonveg} alt="non veg" className="w-8 h-auto " />
+  </div>
+  <div className="flex items-center">
+    <h4 className='text-sm font-bold'>[EXTRA BUTTER RS10 IF APPLICABLE]</h4>  
+  </div>
+
+  <div className=''>
+  <ul>
+  <li className="flex flex-col">
+    <div className="flex justify-between mr-2">
+      <p>Plain Naan</p>
+      <p>₹40</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Roti</p>
+      <p>₹40</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kulcha</p>
+      <p>₹40</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Garlic Naan</p>
+      <p>₹60</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Stuffed Naan</p>
+      <p>₹80</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Tandoori Parotta</p>
+      <p>₹40</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Kashmiri Naan</p>
+      <p>₹150</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Phulka (2pcs)</p>
+      <p>₹50</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Family Naan</p>
+      <p>₹80</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Family Butter Naan</p>
+      <p>₹100</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Cheese Naan</p>
+      <p>₹70</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Masala Kulcha</p>
+      <p>₹60</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Panner Kulcha</p>
+      <p>₹50</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Aloo Parotta</p>
+      <p>₹50</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Methi Parotta</p>
+      <p>₹40</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Latcha Parotta</p>
+      <p>₹80</p>
+    </div>
+    <div className="flex justify-between mr-2">
+      <p>Chicken Keema Naan</p>
+      <p>₹140</p>
+    </div>
+   
+    
+  </li>
+</ul>
+</div>
+    
+  
+
+    
+  </li>
+</ul>
+</div>
+   
+    
+  
+
+    
+  </li>
+</ul>
+</div>
 
 </body>
  
